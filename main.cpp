de8b46c0d7bdae1ec64cd0d1b732e96a8df50061
Mathieu <mathieukerbel@gmail.com>
2024-02-15 15:27:49 +0100

    ressources main

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 
 #include <iostream>
 
+// ressources :
+// https://www.editions.polytechnique.fr/files/pdf/EXT_1332_5.pdf
+// https://fr.wikipedia.org/wiki/A%C3%A9rodynamique_automobile
+
 int main(void){
     std::cout << "Welcome to the Aerodynamics Calculator" << std::endl;
 }
